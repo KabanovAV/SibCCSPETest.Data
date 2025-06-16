d636ba7c065fec0b6e2632f87ba5a5c02f0e48e2
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-06-16 16:57:31 +0600

    Добавлено DTO для группы. Добавлен маппинг группы: из группы в DTO, добавление DTO в группу. Переделан контроллер с применением DTO

--- SibCCSPETest.Data.Interop/DTOs/GroupDTO.cs
+++ SibCCSPETest.Data.Interop/DTOs/GroupDTO.cs
@@ -1,28 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace SibCCSPETest.Data
 {
-    public class Group
+    public class GroupDTO
     {
-        [Key]
         public int Id { get; set; }
         [Display(Name = "Название")]
         [Required(ErrorMessage = "Обязательное поле для заполнения")]
         [StringLength(200, MinimumLength = 10, ErrorMessage = "Количество символов от 10 до 200")]
         public string Title { get; set; } = string.Empty;
-        public int SpecializationId { get; set; }
         [Display(Name = "Начало курсов")]
         [Required(ErrorMessage = "Обязательное поле для заполнения")]
         public DateTime Begin { get; set; }
         [Display(Name = "Окончание курсов")]
         [Required(ErrorMessage = "Обязательное поле для заполнения")]
         public DateTime End { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime ChangedDate { get; set; }
-
-        public Specialization? Specialization { get; set; }
-        public List<GroupUser>? GroupUser { get; set; }
-        public Setting? Setting { get; set; }
     }
 }
